c1661c677ad28b85ee60d3571df58e5cc3271f46
ruslannazari <nazarmuhamedowruslan@gmail.com>
2024-11-04 18:53:10 +0500

    Added command 'start'

--- src/bot.ts
+++ src/bot.ts
@@ -8,6 +8,10 @@ const {
 
 export const bot = new Bot(token)
 
+bot.command("start", async (ctx) => {
+    await ctx.reply("Я бот Рона и развиваюсь по мере его знаний😁")
+})
+
 bot.on('message', async ctx => {
     await ctx.reply(ctx.message.text!)
 })
